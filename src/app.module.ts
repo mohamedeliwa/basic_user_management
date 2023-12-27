1b01b23be63f3eaeee007bd6e793662d98053c98
mohamedeliwa <mohamed.av11@gmail.com>
2023-12-27 20:13:45 +0200

    creating prisma exception custom filter, to show better error message

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { PrismaClientExceptionFilter } from './prisma/prisma.exception.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,6 +16,12 @@ import { PrismaModule } from './prisma/prisma.module';
     PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: PrismaClientExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
